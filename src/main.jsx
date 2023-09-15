d73c9edf5bb190447097da75bbdeda44249965a6
Talita Gabriela Del Poso Vilalva <talitagabriela1216@gmail.com>
2023-09-15 14:19:27 -0400

    Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+//npm i react-router-dom//
+
+import {Routes,Route,BrowserRouter} from "react-router-dom";
+
+import Home from './Home/Home.jsx'
+import ToWach from './ToWatch/ToWatch.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Home/>}></Route>
+      <Route path='/ToWatch' element={<ToWach/>}></Route>
+    </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )

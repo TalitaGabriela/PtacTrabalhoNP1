5974e9896372743a9a5c65c8ec629b9a41f62072
Talita Gabriela Del Poso Vilalva <talitagabriela1216@gmail.com>
2023-10-21 18:14:47 -0400

    Commit

--- src/ToWatch/ToWatch.jsx
+++ src/ToWatch/ToWatch.jsx
@@ -1,69 +1,52 @@
-import { useState } from "react";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import "./style.css";
 
-export default function towatch() {//uso de componente//
-    // uso de 3 props em cada objeto onde cada uma recebe um useState(estado)//
+export default function ToWatch() {
     const listaLocalStorage = JSON.parse(localStorage.getItem("Lista"));
     const [count, setCount] = useState(0);
     const [atividade, setAtividade] = useState("");
-    const [lista, setLista] = useState(listaLocalStorage || []);//useState que utiliza Array//
+    const [lista, setLista] = useState(listaLocalStorage || []);
     const [id, setId] = useState(1);
+    document.title = `Lista Dorama`
 
-    useEffect(() => {localStorage.setItem("Lista", JSON.stringify(lista))}, [lista]);
-
-    function contagem() {
-        useEffect (() => {
-        document.title = `Você clicou ${count} vezes`;
-        });
-    }
+    useEffect(() => {
+        localStorage.setItem("Lista", JSON.stringify(lista));
+        if (count <= 0) { 
+            document.title = `LISTA DORAMA`;
+        } else if (count == 1) {
+            document.title = `SUA LISTA TEM ${count} DORAMA`;
+        } else if (count > 1) { 
+            document.title = `SUA LISTA TEM ${count} DORAMAS`;
+        }
+    }, [lista, count]);
 
     const salvar = (e) => {
         e.preventDefault();
-        setLista([...lista, {
-            atividade: atividade,   
-            id: id
-        }]);
+        setLista([...lista, { atividade: atividade, id: id }]);
         setId(id + 1);
         setAtividade("");
-        setCount(count+1)   
-
-        if (contagem == 0){
-            return "LISTA VAZIA"
-        }
-        if (contagem == 1){
-            return "SUA LISTA TEM " + {count} + " DORAMA" 
-        } 
-        if (contagem > 1){
-            return "SUA LISTA TEM " + {count} + " DORAMAS" 
-        } 
+        setCount(count + 1);
     };
 
     const remover = (id) => {
-        /*setLista(lista.filter((ativ) => (ativ.id !== id ? lista : null)));*/
-        const auxLista = [];
-        lista.map((lista) => {
-            if (lista.id !== id) {
-                auxLista.push(lista);
-            }
-        });
-        setLista(auxLista);
+        setLista(lista.filter((ativ) => ativ.id !== id));
+        setCount(count - 1);
     }
+
     return (
-        <div class="container">
+        <div className="container">
             <h1>Lista de Doramas</h1>
             <form onSubmit={salvar}>
                 <input type="text"
                     value={atividade}
                     onChange={(e) => { setAtividade(e.target.value) }} />
-                <button>ADD</button>
-                <h2>{contagem}</h2>     
+                <button>ADD</button>     
             </form >
             {lista.map((ativ) =>
                 <ul key={ativ.id}>
                     <li>
                         <p>{ativ.atividade}</p>
-                        <button onClick={() => remover(ativ.id) + setCount(count-1)}>REMOVE</button>
+                        <button onClick={() => remover(ativ.id)}>REMOVE</button>
                     </li>
                 </ul>
             )}

185b845d5017dbb073771bf9faa5163af5181f1f
TalitaGabriela <talitagabriela1216@gmail.com>
2023-11-17 14:23:35 -0400

    Commit

--- src/Detalhe/detalhe.jsx
+++ src/Detalhe/detalhe.jsx
@@ -1,11 +1,20 @@
 import { useParams } from "react-router-dom";
+import Card from '../Componentes/Card';
 
 export default function Detalhe(){
     const { id } = useParams();
+    const lista = JSON.parse(localStorage.getItem("Lista"));
+
+    const atividade = lista.filter((objeto)=>{
+    if(objeto.id == id){
+    return objeto;
+    }
+    return null;
+    });
+
+console.log(atividade[0]);
 
     return(
-        <div>
-            <h1>Olá Mundo</h1>
-        </div>
+    <Card atividade={atividade[0]}/>
     );
 }
